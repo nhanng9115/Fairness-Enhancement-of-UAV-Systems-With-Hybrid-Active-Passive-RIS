7a6a05648c0de60448a27eb1ed0cf953af5a4672
nhanng9115 <65878933+nhanng9115@users.noreply.github.com>
2024-05-28 15:34:45 +0300

    Update add_solver.m

--- add_solver.m
+++ add_solver.m
@@ -1,12 +1,8 @@
-% if strcmp(getenv('COMPUTERNAME'),'OY1902032') == 1 || strcmp(getenv('COMPUTERNAME'),'JALAVA1') == 1
-% soft_store = 'C:\Users\thanguye20\OneDrive - Oulun yliopisto\Research\OptSoftware\';
-% soft_store = 'F:\OneDrive - Oulun yliopisto\Research\OptSoftware\';
-soft_store = 'C:\Users\thanguye20\OneDrive - Oulun yliopisto\Research\OptSoftware\';
+soft_store = 'your folder containing YALMIP and MOSEK'; % <--- edit here
 
 pwd
 currentFolder = pwd; % save current folder
-% add CVX
-% cd(strcat(soft_store,'cvx-w64\cvx')); cvx_setup
+
 
 % add YALMIP
 cd(strcat(soft_store,'YALMIP-master')); addpath(genpath(pwd))
@@ -16,12 +12,5 @@ cd(strcat(soft_store,'mosek\9.2\toolbox')); addpath(genpath(pwd))
 mosek_env = [getenv('PATH') strcat(';',soft_store,'mosek\9.2\tools\platform\win64x86\bin')];
 setenv('PATH', mosek_env);
 
-% add Manifold Opt
-% addpath(pwd);
-% cd(strcat(soft_store,'manopt')); 
-% addpath(genpath(pwd));
-% cd(currentFolder)
-
 % back to current folder
 cd(currentFolder)
-% end
